ccf54ce01a68dc0e7c66f07f6107244c1c544cdc
Sam Minot <sminot@fredhutch.org>
2021-06-18 13:05:05 -0700

    Reference the file path directly for MAGeCK Flute

--- bin/mageck_flute_rra.R
+++ bin/mageck_flute_rra.R
@@ -6,12 +6,10 @@ args = commandArgs(trailingOnly=TRUE)
 
 # Set up the paths to the input files
 gene_summary_txt = args[1]
-file1 = file.path(system.file("extdata", package = "MAGeCKFlute"), gene_summary_txt)
 print(gene_summary_txt)
 print(file.exists(gene_summary_txt))
 
 sgrna_summary_txt = args[2]
-file2 = file.path(system.file("extdata", package = "MAGeCKFlute"), sgrna_summary_txt)
 print(sgrna_summary_txt)
 print(file.exists(sgrna_summary_txt))
 
@@ -25,4 +23,4 @@ organism = args[4]
 scale_cutoff = args[5]
 
 # Run the MAGeCK Flute pipeline for the output of MAGeCK test RRA
-FluteRRA(file1, file2, proj=proj, organism=organism, scale_cutoff = scale_cutoff, outdir = getwd())
+FluteRRA(gene_summary_txt, sgrna_summary_txt, proj=proj, organism=organism, scale_cutoff = scale_cutoff, outdir = getwd())
